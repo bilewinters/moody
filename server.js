65d2c306812ecd5c772e708a1c50b313936929c5
pmccabe <paul.mccabe@flextrade.com>
2015-10-20 13:04:48 +0100

    Allowing the server to bind to the port heroku assigns it.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 let express = require('express');
 
 let app  = express();
-let port = 8080;
+let port = (process.env.port || 8080);
 
 app.use(express.static(__dirname + '/dist/'));
 app.listen(port);
